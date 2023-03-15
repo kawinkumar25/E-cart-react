cdb4f7e6f54a7da19862d6c99bfdc808c8658389
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2023-03-15 16:19:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,29 +10,12 @@ import Marvel from './components/Marvel';
 import './styles/amazon.css';
 import list from './data';
 import {Routes,Route } from 'react-router-dom';
-import Cards from './components/Cards';
 import './App.css';
 import Horror from './components/Horror';
 const App = () => {
-	const [show, setShow] = useState(true);
+	
 	 const [cart , setCart] = useState([]);
 	
-	// const handleClick = (item)=>{
-
-	// 	let isPresent = false;
-		
-	// 	cart.forEach((product)=>{
-	// 		if (item.id === product.id)
-	// 		// cart.splice(cart.findIndex(item.id),1);
-	// 		isPresent = true;
-            
-	// 	})
-	// 	if (isPresent){
-			
-	// 		return ;
-	// 	}
-	// 	setCart([...cart, item]);
-	// }
 	const details = [...list];
 	console.log(details);
 
